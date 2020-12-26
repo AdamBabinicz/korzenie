b69b6c21891b52efa637602475c85709bf634e81
hipek <puaro@vp.pl>
2020-12-26 19:10:15 +0100

    zmiany

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -28,29 +28,29 @@ const Footer = () => {
           <FooterLinksWrapper>
             <FooterLinkItems>
               <FooterLinkTitle>About Us</FooterLinkTitle>
-              <FooterLink to="/signin">How it works</FooterLink>
+              {/* <FooterLink to="/signin">How it works</FooterLink>
               <FooterLink to="/signin">Testimonials</FooterLink>
               <FooterLink to="/signin">Careers</FooterLink>
               <FooterLink to="/signin">Investors</FooterLink>
-              <FooterLink to="/signin">Terms od Service</FooterLink>
+              <FooterLink to="/signin">Terms od Service</FooterLink> */}
             </FooterLinkItems>
             <FooterLinkItems>
               <FooterLinkTitle>Contact Us</FooterLinkTitle>
-              <FooterLink to="/signin">How it works</FooterLink>
+              {/* <FooterLink to="/signin">How it works</FooterLink>
               <FooterLink to="/signin">Testimonials</FooterLink>
               <FooterLink to="/signin">Careers</FooterLink>
               <FooterLink to="/signin">Investors</FooterLink>
-              <FooterLink to="/signin">Terms od Service</FooterLink>
+              <FooterLink to="/signin">Terms od Service</FooterLink> */}
             </FooterLinkItems>
           </FooterLinksWrapper>
           <FooterLinksWrapper>
             <FooterLinkItems>
               <FooterLinkTitle>Videos</FooterLinkTitle>
-              <FooterLink to="/signin">How it works</FooterLink>
+              {/* <FooterLink to="/signin">How it works</FooterLink>
               <FooterLink to="/signin">Testimonials</FooterLink>
               <FooterLink to="/signin">Careers</FooterLink>
               <FooterLink to="/signin">Investors</FooterLink>
-              <FooterLink to="/signin">Terms od Service</FooterLink>
+              <FooterLink to="/signin">Terms od Service</FooterLink> */}
             </FooterLinkItems>
             <FooterLinkItems>
               <FooterLinkTitle>Social Media</FooterLinkTitle>
