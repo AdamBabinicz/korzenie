e80df839a3a6008269a0e14aeb6a074af0705fe5
babinicz <babinicz@onet.pl>
2024-02-15 18:52:52 +0100

    zmiany

--- src/components/Signin/pawel.js
+++ src/components/Signin/pawel.js
@@ -17,10 +17,11 @@ const ambrozy = () => {
         </h3>
         <p>
           <b>Paweł</b> pojął za żonę w 1887 Katarzynę Czerwińską. Mieli Józefę,
-          Adama ur. w 1888 (✝1889), Józefa ur. w 1890 (✝1890),
+          Adama ur. w 1888 (✝06.03.1889 - 3 miesiące), Józefa ur. w 1890 (✝1890
+          - 6 tyg.),
           <b> Jana ur. w 1890</b>, Stanisława ur. w 1893, Zofię ur. w 1900,
-          Rozalię (✝1906), Franciszka ur. w 1910 i Stanisławę ur. w 1911
-          (✝1911).
+          Rozalię (✝1906), Franciszka ur. w 1910 i Stanisławę ur. w 1911 (✝1911
+          - 3 tyg.).
         </p>
         <p>Józefa wyszła za mąż w 1928 za wdowca Władysława Walczaka.</p>
         <p>Paweł był dziadkiem mojego ojca Józefa ur. w 1930.</p>
