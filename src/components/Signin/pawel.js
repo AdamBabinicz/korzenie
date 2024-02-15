88d9bd8aca8111e31303f98d964b3be373088514
babinicz <babinicz@onet.pl>
2024-02-15 19:16:25 +0100

    zmiany

--- src/components/Signin/pawel.js
+++ src/components/Signin/pawel.js
@@ -20,8 +20,16 @@ const ambrozy = () => {
           Kieragi. Mieli Józefę, Adama ur. w 1888 (✝06.03.1889 - 3 miesiące),
           Józefa ur. w 1890 (✝1890 - 6 tyg.),
           <b> Jana ur. w 1890</b>, Stanisława ur. w 1893, Zofię ur. w 1900,
-          Rozalię (✝1906), Franciszka ur. w 1910 i Stanisławę ur. w 1911 (✝1911
-          - 3 tyg.).
+          Antoniego ur. w 1903, Rozalię (✝1906), Franciszka ur. w 1910 i
+          Stanisławę ur. w 1911 (✝1911 - 3 tyg.).
+        </p>
+        <p>
+          Antoni wziął za żonę Honoratę Musiałek c. Józefy Banasiak. Ślub odbył
+          się w 1931 w Zakrzewie (Zakrzowie).
+        </p>
+        <p>
+          Franciszek ożenił się w 1932 z Feliksą Szczygłowską c. Karoliny
+          Kozłowskiej. Slub odbył się w parafii Kowala-Stępocina .
         </p>
         <p>Józefa wyszła za mąż w 1928 za wdowca Władysława Walczaka.</p>
         <p>Paweł był dziadkiem mojego ojca Józefa ur. w 1930.</p>
