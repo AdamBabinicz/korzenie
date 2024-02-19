5c18aefc52c6829a9216a65232f7a3a77d4be3b1
babinicz <babinicz@onet.pl>
2024-02-19 18:50:13 +0100

    zmiany

--- src/components/Signin/jan.js
+++ src/components/Signin/jan.js
@@ -21,7 +21,7 @@ const ambrozy = () => {
         <p>
           Małżonkowie mieli Jana (żył 2 dni, ✝1918), Mariannę (żyła 2 tygodnie,
           ✝1918), Janinę (żyła 8 tygodni, ✝1920), Stefana (1921-2017),
-          Władysława, Tadeusza (192x-1974),
+          Władysława (1923-02.12.2004), Tadeusza (192x-1974),
           <b> Józefa (1930-1996)</b> i Zofię (19xx-2022).
         </p>
       </Wrapper>
