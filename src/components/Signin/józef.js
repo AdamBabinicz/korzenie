ca27c660c2abbdec903c12c1b2020f4ff97924cf
babinicz <babinicz@onet.pl>
2024-06-10 20:25:48 +0200

    zm

--- src/components/Signin/józef.js
+++ src/components/Signin/józef.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { Container, Wrapper, Icon } from "./przodekElements";
 import { BsArrowLeftSquareFill } from "react-icons/bs";
 import { DarkOverlayContext } from "../../context/DarkOverlayContext";
-import img from "../../images/48.png";
+import img from "../../images/51.png";
 
 const Józef = () => {
   const { isOverlayVisible } = useContext(DarkOverlayContext);
